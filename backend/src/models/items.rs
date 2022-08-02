21b7486bdc21fe202c8fc0719e6039a841fd01a7
Eero Häihälä <eero.haihala@gmail.com>
2022-08-02 23:31:52 +0300

    Sketch out endoints

--- backend/src/models/items.rs
+++ backend/src/models/items.rs
@@ -1,15 +1,25 @@
-use rocket::serde::Serialize;
+use rocket::serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize)]
 #[serde(crate = "rocket::serde")]
 pub struct Item {
     pub name: &'static str,
+    pub id: Uuid,
     pub price: f32,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+pub struct Order {
+    pub product_id: Uuid,
+    pub quantity: f32,
 }
 
 #[derive(Serialize)]
 #[serde(crate = "rocket::serde")]
 pub struct Cat {
     pub name: &'static str,
+    pub id: Uuid,
     pub image: &'static str,
 }
