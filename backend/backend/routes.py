ea9dbb6719271c2ca73a21d47cab6273b74ce98e
Eero Häihälä <eero.haihala@gmail.com>
2022-08-11 15:37:38 +0300

    Add endpoint to adopt cats

--- backend/backend/routes.py
+++ backend/backend/routes.py
@@ -1,7 +1,7 @@
 from backend.utils import dict_to_model_list
 from backend.models import Cat, Item
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
 from uuid import uuid4
 
@@ -17,27 +17,27 @@ items = {
 
 
 cats = {
-    uuid4(): {
+    str(uuid4()): {
         "name": "Gus, The Theatre Cat",
         "image": "https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg?auto=compress&cs=tinysrgb&w=1600",
     },
-    uuid4(): {
+    str(uuid4()): {
         "name": "Rum Tum Tugger",
         "image": "https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&w=1600",
     },
-    uuid4(): {
+    str(uuid4()): {
         "name": "Mr Mistoffelees",
         "image": "https://images.pexels.com/photos/416160/pexels-photo-416160.jpeg?auto=compress&cs=tinysrgb&w=1600",
     },
-    uuid4(): {
+    str(uuid4()): {
         "name": "Old Deuteronomy",
         "image": "https://images.pexels.com/photos/1170986/pexels-photo-1170986.jpeg?auto=compress&cs=tinysrgb&w=1600",
     },
-    uuid4(): {
+    str(uuid4()): {
         "name": "Grizabella",
         "image": "https://images.pexels.com/photos/320014/pexels-photo-320014.jpeg?auto=compress&cs=tinysrgb&w=1600",
     },
-    uuid4(): {
+    str(uuid4()): {
         "name": "Macavity",
         "image": "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     }
@@ -52,3 +52,11 @@ def get_items():
 @router.get("/cats", response_model=list[Cat])
 async def get_cats():
     return dict_to_model_list(cats)
+
+
+@router.post("/adopt/<uuid>")
+async def get_cats(uuid):
+    if uuid not in cats:
+        raise HTTPException(status_code=404, detail="Item not found")
+    else:
+        del cats[uuid]
--- backend/backend/utils.py
+++ backend/backend/utils.py
@@ -2,4 +2,4 @@ from typing import Any
 
 
 def dict_to_model_list(dict_input: dict[str, dict[str, Any]]) -> list[dict]:
-    return [value | {"id": str(key)} for key, value in dict_input.items()]
+    return [value | {"id": key} for key, value in dict_input.items()]
