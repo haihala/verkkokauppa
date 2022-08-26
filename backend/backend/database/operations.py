35a35404b11f9023e40a2347f1f38fcd3a6d88b6
Eero Häihälä <eero.haihala@gmail.com>
2022-08-26 10:25:19 +0300

    DB based users

--- backend/backend/database/operations.py
+++ backend/backend/database/operations.py
@@ -1,7 +1,29 @@
+from secrets import compare_digest
+
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from sqlalchemy.orm import Session
 
 from .. import schemas
-from . import models
+from . import get_db, models
+
+
+security = HTTPBasic()
+
+
+def get_user_id(db: Session = Depends(get_db), credentials: HTTPBasicCredentials = Depends(security)) -> str:
+    if user := db.query(models.User).filter(
+        models.User.username ==
+        credentials.username,
+        models.User.password == credentials.password,
+    ).first():
+        return user.id
+
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Incorrect email or password",
+        headers={"WWW-Authenticate": "Basic"},
+    )
 
 
 def get_products(db: Session):
@@ -32,7 +54,7 @@ def cat_exists(db: Session, cat_id: int) -> bool:
     return db.query(models.Cat).filter(models.Cat.id == cat_id).first() is not None
 
 
-def adopt_cat(db: Session, cat_id: int, user: int):
+def adopt_cat(db: Session, cat_id: int, user: str):
     db.query(models.Cat).filter(
         models.Cat.id == cat_id
     ).update({"owner_id": user})
--- backend/backend/routes.py
+++ backend/backend/routes.py
@@ -4,7 +4,6 @@ from fastapi import APIRouter, HTTPException, Depends, status
 
 from .database import get_db, operations
 from .schemas import Cat, Product, Order
-from .login import get_user_id
 
 
 router = APIRouter()
@@ -22,7 +21,7 @@ async def get_products(db: Session = Depends(get_db)):
         404: {"description": "An invalid product in the order"},
     },
 )
-async def buy(orders: list[Order], user_id: int = Depends(get_user_id), db: Session = Depends(get_db)):
+async def buy(orders: list[Order], user_id: str = Depends(operations.get_user_id), db: Session = Depends(get_db)):
     if all(operations.product_exists(db, order.product) for order in orders):
         return operations.order_products(db, orders, user_id)
     raise HTTPException(status_code=404, detail="Item not found")
@@ -39,7 +38,7 @@ async def get_cats(db: Session = Depends(get_db)):
         404: {"description": "The cat was not found"},
     },
 )
-async def adopt_cat(cat_id: str, user_id: int = Depends(get_user_id), db: Session = Depends(get_db)):
+async def adopt_cat(cat_id: str, user_id: str = Depends(operations.get_user_id), db: Session = Depends(get_db)):
     if operations.cat_exists(db, cat_id):
         return operations.adopt_cat(db, cat_id, user_id)
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
